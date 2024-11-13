6e418831db1d4bebd64f2e44c51a0c6045d2f1da
Lenilson Soares <lenilsondiassoares@gmail.com>
2024-11-13 13:47:25 -0300

    comit 1

--- server/models/Goal.js
+++ server/models/Goal.js
@@ -1,43 +1,56 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db');
+const { connectDB } = require('../config/db');
 
-const Goal = sequelize.define('Goal', {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    titulo: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    curso_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'Cursos',
-            key: 'id'
+const Goal = {
+    create: async (titulo, curso_id, dia, duracao, usuario_id) => {
+        const db = await connectDB();
+        try {
+            const query = 'INSERT INTO Metas (titulo, curso_id, dia, duracao, usuario_id) VALUES (?, ?, ?, ?, ?)';
+            const [results] = await db.execute(query, [titulo, curso_id, dia, duracao, usuario_id]);
+            return results;
+        } finally {
+            db.release();
         }
     },
-    dia: {
-        type: DataTypes.STRING,
-        allowNull: false
+    findAll: async () => {
+        const db = await connectDB();
+        try {
+            const query = 'SELECT * FROM Metas';
+            const [results] = await db.execute(query);
+            return results;
+        } finally {
+            db.release();
+        }
     },
-    duracao: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+    findById: async (id) => {
+        const db = await connectDB();
+        try {
+            const query = 'SELECT * FROM Metas WHERE id = ?';
+            const [results] = await db.execute(query, [id]);
+            return results[0];
+        } finally {
+            db.release();
+        }
     },
-    usuario_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'Usuarios',
-            key: 'id'
+    update: async (id, titulo, curso_id, dia, duracao, usuario_id) => {
+        const db = await connectDB();
+        try {
+            const query = 'UPDATE Metas SET titulo = ?, curso_id = ?, dia = ?, duracao = ?, usuario_id = ? WHERE id = ?';
+            const [results] = await db.execute(query, [titulo, curso_id, dia, duracao, usuario_id, id]);
+            return results;
+        } finally {
+            db.release();
+        }
+    },
+    delete: async (id) => {
+        const db = await connectDB();
+        try {
+            const query = 'DELETE FROM Metas WHERE id = ?';
+            const [results] = await db.execute(query, [id]);
+            return results;
+        } finally {
+            db.release();
         }
     }
-}, {
-    timestamps: true,
-    tableName: 'Metas'
-});
+};
 
 module.exports = Goal;
